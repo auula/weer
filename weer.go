b9bde03b7af8b113e169f43ecfcd1e7d2b643b1a
Dings <deen.job@qq.com>
2020-11-05 22:13:44 +0800

    add all source file.

--- weer.go
+++ weer.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const (
 	// Banner is application logo
-	Banner = `
-		________                   
-		|  |  |  |.-----.-----.----.
+	Banner = `					 
+		 ________                  
+		|  |  |  |.-----.-----.----. 
 		|  |  |  ||  -__|  -__|   _|
 		|________||_____|_____|__| 
 	`
@@ -16,8 +18,21 @@ const (
 	AppVer = "0.0.1 BETA"
 )
 
+var (
+	client = &http.Client{}
+)
+
 func main() {
 	fmt.Println(Banner)
 	fmt.Println(AppVer)
 	fmt.Println("Weer is weather command line apps.")
+	req, err := http.NewRequest("GET", "https://wttr.in", nil)
+	req.Header.Add("Content-Type", `text/plain`)
+	resp, err := client.Do(req)
+	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(body))
 }
